da86b359b2d75c53eb55bf9a47a56a05a61ed732
sdmoraes22 <cmoraes.ti@gmail.com>
2020-10-25 17:23:06 -0300

    Implementação do contexto do catálogo

---
+++ src/building blocks/NSE.Core/Data/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSE.Core.Data
+{
+    public interface IRepository<T> : IDisposable where T : IAggregateRoot
+    {
+    }
+}
