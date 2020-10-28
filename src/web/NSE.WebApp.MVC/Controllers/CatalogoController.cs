ba1b7b157d88aa86b7c0e0252851c36d0cb71111
sdmoraes22 <cmoraes.ti@gmail.com>
2020-10-28 19:43:02 -0300

    Implementação do serviço de http de catálogo para consumo da API no MVC

---
+++ src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Services;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private readonly ICatalogoService _catalogoService;
+        public CatalogoController(ICatalogoService catalogoService)
+        {
+            _catalogoService = catalogoService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _catalogoService.ObterTodos();
+            return View(produtos);
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            var produto = await _catalogoService.ObterPorId(id);
+            return View(produto);
+        }
+    }
+}
