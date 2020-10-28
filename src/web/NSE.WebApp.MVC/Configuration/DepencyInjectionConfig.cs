bbdec2966aee516267cef9e04052f803200bfa92
sdmoraes22 <cmoraes.ti@gmail.com>
2020-10-28 20:23:08 -0300

    Criação do delegating handler para poder enviar o Bearer no request do MVC

--- src/web/NSE.WebApp.MVC/Configuration/DepencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DepencyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
+using NSE.WebApp.MVC.Services.Handlers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,8 +15,12 @@ namespace NSE.WebApp.MVC.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+            
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>();
+            
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             
