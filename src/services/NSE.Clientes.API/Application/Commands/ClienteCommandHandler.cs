ba57e5a953c4d593e7c6b232d5c596e062f29ca7
sdmoraes22 <cmoraes.ti@gmail.com>
2020-11-05 18:03:26 -0300

    Estrutura base de comando para cliente

---
+++ src/services/NSE.Clientes.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            // validar cmd
+            // persistir na base
+        }
+    }
+}
