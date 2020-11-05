5cbda4a0a27889004fd5bd9274696d984beb67ee
sdmoraes22 <cmoraes.ti@gmail.com>
2020-11-04 21:59:04 -0300

    Definição das entidades de domínio

---
+++ src/building blocks/NSE.Core/DomainObjects/Email.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Email
+    {
+
+        public const int EnderecoMaxLength = 254;
+        public const int EnderecoMinLength = 5;
+        public string Endereco { get; set; }
+
+        protected Email() { }
+
+        public Email(string endereco)
+        {
+            if (!Validar(endereco)) throw new DomainException("Email Inválido");
+            Endereco = endereco;
+        }
+
+        public static bool Validar(string email)
+        {
+            var regexEmail = new Regex(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+            return regexEmail.IsMatch(email);
+
+        }
+
+
+
+    }
+}
