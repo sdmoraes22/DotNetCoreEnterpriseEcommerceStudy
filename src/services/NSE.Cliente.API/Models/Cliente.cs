f8b7253d4644d77b53903ec264b36b84b0070d45
sdmoraes22 <cmoraes.ti@gmail.com>
2020-11-04 21:02:00 -0300

    Definição inicial das entidades.

---
+++ src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string CPF { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        public Cliente(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            CPF = cpf;
+            Excluido = false;
+        }
+
+        //EF Rellation
+        protected Cliente() { }
+    }
+}
